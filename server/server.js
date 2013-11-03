5c6a2e26f0c2b5f11bf8e175cf5010b5b2aa5569
Achal Dave <achalddave@live.com>
2013-11-03 05:53:36 +0000

    stupid express bug fix?

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ var WebSocketServer = require('ws').Server
   , app = express()
   , port = process.env.PORT || 5000;
 
-app.use(express.bodyParser());
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(express.static(__dirname + '/'));
 
 var candleState = 1;
